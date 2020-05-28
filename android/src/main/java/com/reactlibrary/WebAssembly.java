696f1e859e0cfbd0d798d8a4fa5d4072628b0c1e
tiannian <dtiannian@aliyun.com>
2020-05-28 23:38:29 +0800

    add symbol

--- android/src/main/java/com/reactlibrary/WebAssembly.java
+++ android/src/main/java/com/reactlibrary/WebAssembly.java
@@ -4,6 +4,7 @@ import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactMethod;
 import com.facebook.react.bridge.Promise;
+import com.facebook.react.bridge.WritableMap;
 import java.util.Base64;
 
 public class WebAssembly extends ReactContextBaseJavaModule {
